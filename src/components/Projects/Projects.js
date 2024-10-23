e5e6cde8447ba2f926e4f0775d51204dc1637f6b
ieee-cisrec <ieee.cis@rajalakshmi.edu.in>
2024-10-23 11:54:40 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -71,55 +71,55 @@ function Projects() {
             />
           </Col>
 
-          // <div
-          //   className="fade-in-element q_a fade-in"
-          //   ref={(el) => (fadeInElements.current[1] = el)}
-          // >
-          //   <h1 className="project-heading">
-          //     F <strong className="purple">A </strong>Q
-          //   </h1>
-          //   <Accordion defaultActiveKey="4" className="bg-transparent">
-          //     <Accordion.Item eventKey="0">
-          //       <Accordion.Header>Accordion Item #1</Accordion.Header>
-          //       <Accordion.Body>
-          //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          //         do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          //         Ut enim ad minim veniam, quis nostrud exercitation ullamco
-          //         laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          //         irure dolor in reprehenderit in voluptate velit esse cillum
-          //         dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          //         cupidatat non proident, sunt in culpa qui officia deserunt
-          //         mollit anim id est laborum.
-          //       </Accordion.Body>
-          //     </Accordion.Item>
-          //     <Accordion.Item eventKey="1">
-          //       <Accordion.Header>Accordion Item #2</Accordion.Header>
-          //       <Accordion.Body>
-          //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          //         do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          //         Ut enim ad minim veniam, quis nostrud exercitation ullamco
-          //         laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          //         irure dolor in reprehenderit in voluptate velit esse cillum
-          //         dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          //         cupidatat non proident, sunt in culpa qui officia deserunt
-          //         mollit anim id est laborum.
-          //       </Accordion.Body>
-          //     </Accordion.Item>
-          //     <Accordion.Item eventKey="2">
-          //       <Accordion.Header>Accordion Item #2</Accordion.Header>
-          //       <Accordion.Body>
-          //         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-          //         do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          //         Ut enim ad minim veniam, quis nostrud exercitation ullamco
-          //         laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          //         irure dolor in reprehenderit in voluptate velit esse cillum
-          //         dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-          //         cupidatat non proident, sunt in culpa qui officia deserunt
-          //         mollit anim id est laborum.
-          //       </Accordion.Body>
-          //     </Accordion.Item>
-          //   </Accordion>
-          // </div>
+          <div
+            className="fade-in-element q_a fade-in"
+            ref={(el) => (fadeInElements.current[1] = el)}
+          >
+            // <h1 className="project-heading">
+            //   F <strong className="purple">A </strong>Q
+            // </h1>
+            // <Accordion defaultActiveKey="4" className="bg-transparent">
+            //   <Accordion.Item eventKey="0">
+            //     <Accordion.Header>Accordion Item #1</Accordion.Header>
+            //     <Accordion.Body>
+            //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            //       do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            //       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            //       laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            //       irure dolor in reprehenderit in voluptate velit esse cillum
+            //       dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            //       cupidatat non proident, sunt in culpa qui officia deserunt
+            //       mollit anim id est laborum.
+            //     </Accordion.Body>
+            //   </Accordion.Item>
+            //   <Accordion.Item eventKey="1">
+            //     <Accordion.Header>Accordion Item #2</Accordion.Header>
+            //     <Accordion.Body>
+            //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            //       do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            //       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            //       laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            //       irure dolor in reprehenderit in voluptate velit esse cillum
+            //       dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            //       cupidatat non proident, sunt in culpa qui officia deserunt
+            //       mollit anim id est laborum.
+            //     </Accordion.Body>
+            //   </Accordion.Item>
+            //   <Accordion.Item eventKey="2">
+            //     <Accordion.Header>Accordion Item #2</Accordion.Header>
+            //     <Accordion.Body>
+            //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            //       do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            //       Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            //       laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            //       irure dolor in reprehenderit in voluptate velit esse cillum
+            //       dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            //       cupidatat non proident, sunt in culpa qui officia deserunt
+            //       mollit anim id est laborum.
+            //     </Accordion.Body>
+            //   </Accordion.Item>
+            // </Accordion>
+          </div>
         </Row>
       </Container>
     </Container>
